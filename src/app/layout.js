d790ac5fb26b1f2bdd020ee09b7d3872b1bd2e80
NHossen <naeemhossen2509@gmail.com>
2024-09-05 01:50:00 +0400

    home page layout updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar></Navbar>
+        <div></div>
         {children}
         
         </body>
